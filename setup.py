7fde2ccb7507964a331544e81d1257c993780b78
kevinkk525 <kevinkk525@users.noreply.github.com>
2016-10-26 00:04:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 
@@ -12,7 +12,7 @@ setup(name='python-bittrex',
       author_email='eric@corsairconsulting.com',
       classifiers=[
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
           'Operating System :: OS Independent',
           'License :: OSI Approved :: MIT License',
           'Development Status :: 3 - Alpha',
